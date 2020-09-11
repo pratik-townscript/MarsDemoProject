a47a32e66c79b42247ed60446b9e6286751f96ea
Ashish-Chavan <ashish.chavan@townscript.com>
2020-09-11 17:16:19 +0530

    print output logic

--- src/main/java/com/townscript/marsproject/App.java
+++ src/main/java/com/townscript/marsproject/App.java
@@ -1,12 +1,10 @@
 package com.townscript.marsproject;
 
+import static com.townscript.marsproject.Point.P;
+import java.util.List;
 import java.util.Scanner;
-import static com.townscript.marsproject.Point.*;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
 
   Matrix matrix;
@@ -19,6 +17,59 @@ public class App {
   public static void main(String[] args) {
     App app = new App();
     app.processInput();
+    app.provideResult();
+  }
+
+  private void provideResult() {
+    ShortestPath usShortestPath = matrix.shortestPath(usStart, destination);
+
+    if (usShortestPath == null) {
+      printUnreachableDesOutput();
+    } else {
+      printOutput(usShortestPath, usInitialDirection);
+    }
+
+    ShortestPath ruShortestPath = matrix.shortestPath(ruStart, destination);
+
+    if (ruShortestPath == null) {
+      printUnreachableDesOutput();
+    } else {
+      printOutput(ruShortestPath, ruInitialDirection);
+    }
+
+    printWinner(usShortestPath, ruShortestPath);
+
+  }
+
+  private void printUnreachableDesOutput() {
+    System.out.println(-1);
+    System.out.println("NA");
+  }
+
+  private void printOutput(ShortestPath shortestPath, String initialDirection) {
+    System.out.println(shortestPath.distance);
+    System.out.println(this.getPath(shortestPath.getRoute(), initialDirection));
+  }
+
+  private void printWinner(ShortestPath usShortestPath, ShortestPath ruShortestPath) {
+
+    try {
+      if (usShortestPath.distance == ruShortestPath.distance) {
+        System.out.println("TIE");
+      } else if (usShortestPath.distance > ruShortestPath.distance) {
+        System.out.println("RUSSIA");
+      } else {
+        System.out.println("US");
+      }
+    } catch (NullPointerException e) {
+      if (usShortestPath == null && ruShortestPath != null) {
+        System.out.println("RUSSIA");
+      } else if (usShortestPath != null && ruShortestPath == null) {
+        System.out.println("US");
+      } else {
+        System.out.println("TIE");
+      }
+    }
   }
 
   private void processInput() {
@@ -52,7 +103,99 @@ public class App {
     int ruStartY = in.nextInt();
     ruStart = P(ruStartX, ruStartY);
     ruInitialDirection = in.next();
-    System.out.println("Russiona position is " + ruStart + " intial dir is " + ruInitialDirection);
+    System.out.println("Russio position is " + ruStart + " intial dir is " + ruInitialDirection);
     in.close();
   }
+
+
+  public String getPath(List<Point> points, String startDirection) {
+    String path = "";
+    String strtDir = startDirection;
+    for (int i = 0; i < points.size() - 1; i++) {
+      Point start = points.get(i);
+      Point end = points.get(i + 1);
+      int xdis = end.x - start.x;
+      int ydis = end.y - start.y;
+      path = path.concat(getMoves(xdis, ydis, strtDir));
+      strtDir = getNextDirection(xdis, ydis);
+    }
+    if (path.isEmpty()) {
+      path = "NA";
+    }
+    return path;
+  }
+
+  private String getNextDirection(int xdis, int ydis) {
+    if (xdis == 1 && ydis == 0) {
+      return "E";
+    } else if (xdis == -1 && ydis == 0) {
+      return "W";
+    } else if (xdis == 0 && ydis == 1) {
+      return "S";
+    } else if (xdis == 0 && ydis == -1) {
+      return "N";
+    }
+    return "";
+  }
+
+  private String getMoves(int xdis, int ydis, String startDirection) {
+    switch (startDirection) {
+      case "E":
+        return getMovesForStartDirEast(xdis, ydis);
+      case "W":
+        return getMovesForStartDirWest(xdis, ydis);
+      case "N":
+        return getMovesForStartDirNorth(xdis, ydis);
+      case "S":
+        return getMovesForStartDirSouth(xdis, ydis);
+      default:
+        return "";
+    }
+  }
+
+  private String getMovesForStartDirEast(int xdis, int ydis) {
+    if (xdis == 1 && ydis == 0)
+      return "M";
+    else if (xdis == -1 && ydis == 0)
+      return "RRM";
+    else if (xdis == 0 && ydis == 1)
+      return "RM";
+    else
+      return "LM";
+  }
+
+  private String getMovesForStartDirWest(int xdis, int ydis) {
+    if (xdis == 1 && ydis == 0)
+      return "RRM";
+    else if (xdis == -1 && ydis == 0)
+      return "M";
+    else if (xdis == 0 && ydis == 1)
+      return "LM";
+    else
+      return "RM";
+  }
+
+  private String getMovesForStartDirNorth(int xdis, int ydis) {
+    if (xdis == 1 && ydis == 0)
+      return "RM";
+    else if (xdis == -1 && ydis == 0)
+      return "LM";
+    else if (xdis == 0 && ydis == 1)
+      return "LLM";
+    else
+      return "M";
+  }
+
+  private String getMovesForStartDirSouth(int xdis, int ydis) {
+    if (xdis == 1 && ydis == 0)
+      return "LM";
+    else if (xdis == -1 && ydis == 0)
+      return "RM";
+    else if (xdis == 0 && ydis == 1)
+      return "M";
+    else
+      return "LLM";
+  }
+
+
 }
