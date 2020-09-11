459c594dd718d0a967e7d1b811f90c890ac70225
Pratik Patwari <pratik.patwari@townscript.com>
2020-09-11 17:12:08 +0530

    shortest path logic written

--- src/main/java/com/townscript/marsproject/Matrix.java
+++ src/main/java/com/townscript/marsproject/Matrix.java
@@ -1,5 +1,10 @@
 package com.townscript.marsproject;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Matrix {
 
   int xMax;
@@ -40,7 +45,46 @@ public class Matrix {
     grid[x][y] = -1;
   }
 
-  public ShortestPath shortestPath(Point source, Point destination) {
+  public boolean isValid(Point p) {
+    return (p.x >= 0) && (p.x < this.xMax) && (p.y >= 0) && (p.y < this.yMax);
+  }
+
+
+  public ShortestPath shortestPath(Point src, Point dest) {
+    if (grid[src.x][src.y] == -1 || grid[dest.x][dest.y] == -1) {
+      return null;
+    }
+    boolean[][] visited = new boolean[xMax][yMax];
+    visited[src.x][src.y] = true;
+
+    Queue<PathNode> queue = new LinkedList<PathNode>();
+    PathNode srcNode = new PathNode(src, 0, new ArrayList<Point>());
+    queue.add(srcNode);
+
+    while (!queue.isEmpty()) {
+      PathNode currNode = queue.peek();
+      Point pt = currNode.getCurrentpoint();
+      currNode.getPathPoints().add(pt);
+
+      if (pt.x == dest.x && pt.y == dest.y) {
+        return new ShortestPath(currNode.getDistFromSrc(), currNode.getPathPoints());
+      }
+      queue.remove();
+      for (int i = 0; i < Point.NEIGHBORS.length; i++) {
+        Point neighbor = pt.getNegibour(Point.NEIGHBORS[i]);
+
+        if (isValid(neighbor) && grid[neighbor.x][neighbor.y] != -1
+            && !visited[neighbor.x][neighbor.y]) {
+          visited[neighbor.x][neighbor.y] = true;
+          List<Point> routes = new ArrayList<Point>();
+          routes.addAll(currNode.getPathPoints());
+
+          PathNode adjNode = new PathNode(neighbor, currNode.getDistFromSrc() + 1, routes);
+          queue.add(adjNode);
+        }
+      }
+      currNode.setPathPoints(new ArrayList<Point>());
+    }
     return null;
   }
 }
--- src/main/java/com/townscript/marsproject/Point.java
+++ src/main/java/com/townscript/marsproject/Point.java
@@ -1,8 +1,20 @@
 package com.townscript.marsproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Point {
-  private int x;
-  private int y;
+  final int x;
+  final int y;
+
+  public static final Point[] NEIGHBORS = new Point[4];
+
+  static {
+    NEIGHBORS[0] = P(-1, 0);
+    NEIGHBORS[1] = P(0, -1);
+    NEIGHBORS[2] = P(0, 1);
+    NEIGHBORS[3] = P(1, 0);
+  }
 
   public Point(int x, int y) {
     this.x = x;
@@ -13,20 +25,9 @@ public class Point {
     return new Point(x, y);
   }
 
-  public int getX() {
-    return x;
-  }
-
-  public void setX(int x) {
-    this.x = x;
-  }
-
-  public int getY() {
-    return y;
-  }
-
-  public void setY(int y) {
-    this.y = y;
+  public Point getNegibour(Point neighbourPoint) {
+    Point neig = P(this.x + neighbourPoint.x, this.y + neighbourPoint.y);
+    return neig;
   }
 
   @Override
